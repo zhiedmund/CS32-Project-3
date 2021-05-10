5eeb76041ba7c6eea64516de1a4730e3caea0317
Edmund <ninjataco127@gmail.com>
2021-05-10 00:23:14 -0700

    Initial Commit

---
+++ Project3/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Project3
+//
+//  Created by Edmund Zhi on 5/10/21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
